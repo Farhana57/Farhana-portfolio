c2710f61936915b33eb3055ccaffcfeeb75f24f2
farhana57 <farhanakhatun22@gmail.com>
2026-09-20 00:10:35 +0600

    Update config and global css for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/Farhana-portfolio',
-  assetPrefix: '/Farhana-portfolio/',
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
